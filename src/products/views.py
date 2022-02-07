e9ecbf81cb02f3036ab25f0dffd3c117b7c705b7
Abdaallah0407 <adilettulebaev@gmail.com>
2022-02-07 19:07:32 +0600

    host

--- src/products/views.py
+++ src/products/views.py
@@ -186,14 +186,13 @@ class NextPreviouTable(viewsets.ModelViewSet):
                 title__contains="%s M" % str(i-1)).first()
             table_prod = Table_Product.objects.filter(
                 title__contains="%s M" % str(i)).first()
-            
+            if table_prod.is_solid:
+                break
             table_prod.totality = prev_mon_table_prod.totality + counts
             table_prod.price_device = table_prod.totality * table_prod.price
             if table_prod.price_per_quantity:
                 table_prod.price_per_quantity = table_prod.price_device * device_item.quantity
             # table_prod.price_per_quantity = tableprod.price_device * device_item.quantity
-            if table_prod.is_solid:
-                break
 
             table_prod.save()
         tableprod.save()
