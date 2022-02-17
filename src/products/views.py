5e16094e38e8c82c4edca62d3f059952b1049c7a
Abdaallah0407 <adilettulebaev@gmail.com>
2022-02-17 15:43:32 +0600

    host

--- src/products/views.py
+++ src/products/views.py
@@ -137,15 +137,29 @@ class APITableProductUpdateViewSet(viewsets.ModelViewSet):
     serializer_class = TableProductListSerializer
 
     def update(self, request, *args, **kwargs):
-        # get_id = self.request.query_params.get('get_id')
-        # table_product = Table_Product.objects.filter(pk=*get_id).first()
+
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
         table_product = self.get_object()
         table_product.price_device = table_product.totality * table_product.price
         table_product.save()
-        # for table_product in table_products:
-        #     table_product.price_device = table_product.totality * table_product.price
-        #     table_product.save()
-        return Response({"Success": "Created"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.data)
+
+        def perform_update(self, serializer):
+            serializer.save()
+
+        def partial_update(self, request, *args, **kwargs):
+            kwargs['partial'] = True
+            return self.update(request, *args, **kwargs)
 
 
 class APITableProductViewSet(viewsets.ModelViewSet):
