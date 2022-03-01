7ed356a668249c3473c40b37913a77db82026c5d
Abdaallah0407 <adilettulebaev@gmail.com>
2022-03-01 16:44:19 +0600

    news

--- src/contacts/apps.py
+++ src/contacts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ContactsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.contacts'
+    verbose_name = "Обратный Звонок"
--- src/news/admin.py
+++ src/news/admin.py
@@ -1,8 +1,15 @@
+from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import *
 
 # Register your models here.
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description')
+    list_display = ('id', 'title', 'createdAt', 'get_image')
     list_filter = ('title', 'description')
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
+
+    get_image.short_description = "Миниатюра"
