817d92745f10c2eb0a4a675036e1c5af349d7c26
Abdaallah0407 <adilettulebaev@gmail.com>
2022-03-01 16:49:03 +0600

    news

--- src/news/admin.py
+++ src/news/admin.py
@@ -7,9 +7,11 @@ from .models import *
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'createdAt', 'get_image')
+    list_display_links = ['title']
     list_filter = ('title', 'description')
+    readonly_fields = ("get_image",)
 
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
+        return mark_safe(f'<img src={obj.image.url} width="40" height="60">')
 
     get_image.short_description = "Миниатюра"
