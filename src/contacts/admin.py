d74f455549d75af58310b32d1eb217d78aaf5690
Abdaallah0407 <adilettulebaev@gmail.com>
2022-03-01 17:39:33 +0600

    news

--- src/contacts/admin.py
+++ src/contacts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(BackCall)
+class BackCallAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'data_added')
+    list_filter = ('name', 'phone',)
+    list_display_links = ('name',)
