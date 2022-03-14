b3d3f9058ea065338b6b658aee378d31e100c5cd
Abdaallah0407 <adilettulebaev@gmail.com>
2022-03-14 12:26:38 +0600

    contacts

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -5,6 +5,9 @@ class BackCall(models.Model):
 
     name = models.CharField("Имя", max_length=100)
     phone = models.CharField("Номер телефона", max_length=25)
+    email = models.EmailField("Электронный адресс", null=True )
+    theme = models.TextField("Тема", null=True, blank=True)
+    message = models.TextField("Вопрос или пожeлание", null=True, blank=True)
     data_added = models.DateTimeField("Дата добавления", auto_now_add=True)
 
     def __str__(self):
